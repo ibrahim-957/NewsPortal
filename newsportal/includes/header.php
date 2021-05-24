e237b0c4633fe76ef81dd6478396da1002ca4c98
ibrahim-957 <n.naghiyev.ibrahim@gmail.com>
2021-05-24 23:22:23 +0400

    css upgrade

--- newsportal/includes/header.php
+++ newsportal/includes/header.php
@@ -1,23 +1,39 @@
- <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+<nav class=" navbar fixed-top navbar-expand-lg navbar-dark fixed-top" style="background-color: red;">
       <div class="container">
-        <a class="navbar-brand" href="index.php"><img src="images/logo.png" height="50"></a>
+        <a class="navbar-brand" href="index.php" style="background-color: green;"><img src="images/logo.png" height="50"></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="about-us.php">About</a>
+              <a style="color: white;background-color: black;" class="nav-link" href="about-us.php">About</a>
             </li>
                  <li class="nav-item">
-              <a class="nav-link" href="index.php">News</a>
+              <a style="color: white;background-color: black;" class="nav-link ml-4" href="index.php">News</a>
+
             </li>
              <li class="nav-item">
-              <a class="nav-link" href="contact-us.php">Contact us</a>
+              <a style="color: white;background-color: black;" class="nav-link ml-4" href="contact-us.php">Contact us</a>
             </li>
-  
-  
+
+
           </ul>
         </div>
+
       </div>
+
+
+            <div>
+                   <form name="search" action="search.php" method="post">
+              <div class="input-group">
+
+        <input type="text" name="searchtitle" class="form-control" placeholder="Search for..." required>
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="submit" style="background-color: green;">Go!</button>
+                </span>
+              </form>
+              </div>
+
+
     </nav>
